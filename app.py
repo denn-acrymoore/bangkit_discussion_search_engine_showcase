e6cd02049b8d36c2a877ad64fec680d5a3b100b5
denn-acrymoore <denn.sebastian.e@gmail.com>
2022-05-25 14:29:09 +0700

    Implemented Read JSON Dummy Data
    Implemented code to read the data from the discussion_dummy_data.json
    and show it in the webpage during GET request.
    
    Changes:
    - Change the datatype of "keywords" in discussion_dummy_data.json from
      string to array.
    - Added init_dummy_data() in app.py to read the JSON dummy data
      (discussion_dummy_data.json).
    - Implemented the data looping in the main_layout.html.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
+import json
+import os
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
@@ -8,11 +10,36 @@ def index():
 
 @app.route('/ml-demo', methods=['GET', 'POST'])
 def ml_demo():
-    return render_template('ml_demo.html')
+    if request.method == 'GET':
+        return render_template('ml_demo.html', discussions=dummy_data)
+
+    if request.method == 'POST':
+        pass
 
 @app.route('/algorithm-demo', methods=['GET', 'POST'])
 def algorithm_demo():
-    return render_template('algorithm_demo.html')
+    if request.method == 'GET':
+        return render_template('algorithm_demo.html', discussions=dummy_data)
+
+    if request.method == 'POST':
+        pass
+
+def init_dummy_data():
+    # Get the JSON Path:
+    # NOTE: __file__ is the absolute path of this app.py
+    # NOTE: os.pardir is the parent directory (..)
+    json_path = os.path.join(__file__, os.pardir, "discussion_dummy_data.json")
+    print(json_path)
+
+    # Open the JSON Dummy Data:
+    with open(json_path, 'r') as file:
+        dummy_data = json.load(file)
+
+    return dummy_data
 
 if __name__ == "__main__":
+    # Initialize the JSON dummy data:
+    dummy_data = init_dummy_data()
+
+    # Run the Flask App:
     app.run(debug=True, port=5000)
